45655a9bc8cff68394de40c46c260bc4c7eba92b
Abdullah <abdallahmosbah25@gmail.com>
2024-01-11 07:41:17 +0200

    Task 1v 0.1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
-""" creating some backups """
+"""some backups"""
 
 from fabric.api import local
 from datetime import datetime
 
+
 def do_pack():
-    """ generating .tgz archive """
-    local("mkdir -p versions")
-
-    tdate = datetime.now().strftime("%Y%m%d%H%M%S")
-    
-    file_name = "versions/web_static_{}.tgz".format(tdate)
-
+    """pack a folder"""
     try:
-        locals("tar -cvzf {} webstatic".format(file_name))
-        return file_name
-    
-    except Exception as excetion:
+        tdate = datetime.now().strftime('%Y%m%d%H%M%S')
+        fName = f'web_static_{tdate}.tgz'
+        local('mkdir -p versions')
+        local(f'tar -cvzf versions/{fName} web_static')
+        return f'versions/{fName}'
+    except Exception:
         return None
