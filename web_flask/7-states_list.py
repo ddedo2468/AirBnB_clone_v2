2cfab87f3f6577b841da41cbc235e56954058e06
Abdullah <abdallahmosbah25@gmail.com>
2024-01-27 20:22:27 +0200

    sss

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -7,10 +7,11 @@ from models import storage
 
 app = Flask(__name__)
 
+
 @app.route("/states_list", strict_slashes=False)
 def states_list():
     """ list of all states """
-    states = storage.all(State)
+    states = storage.all("State")
     return render_template("7-states_list.html", states=states)
 
 
@@ -18,6 +19,7 @@ def states_list():
 def teardown(exc):
     """ close sql session """
     storage.close()
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
