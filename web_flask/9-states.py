5b59fe596773868d747241bcdf31470b5b0e6624
Abdullah <abdallahmosbah25@gmail.com>
2024-01-28 08:51:52 +0200

    fa

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,32 +1,40 @@
 #!/usr/bin/python3
-""" Simple Flask app. """
+"""script that starts a Flask web application"""
+from flask import Flask, render_template, redirect, url_for
+
 from models import storage
-from flask import Flask, render_template
+from models.state import State
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route("/states", strict_slashes=False)
+@app.route('/states')
 def states():
-    """HTML Page"""
-    states = storage.all(State)
-    return render_template("9-states.html", state=states)
+    """Get all state data"""
+    data = storage.all(State)
+    return render_template("9-states.html",
+                           states=data)
 
 
-@app.route("/states/<id>", strict_slashes=False)
-def states_id(id):
-    """State Page"""
+@app.route('/states/<id>')
+def states_by_id(id):
+    obj = None
+    notfound = True
     for state in storage.all(State).values():
         if state.id == id:
-            return render_template("9-states.html", state=state)
-    return render_template("9-states.html")
+            obj = state
+            notfound = False
+            break
+    return render_template("9-states.html", id=id,
+                           state=obj, notfound=notfound)
 
 
 @app.teardown_appcontext
-def teardown(exc):
-    """tearDown"""
+def terminate(exc):
+    """Close SQLAlchemy session"""
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
